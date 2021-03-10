23ae0efd16ded44bf8ce05d604896e190ac29c47
Franz Decinal <dfranzzzzz05@gmail.com>
2021-03-10 17:34:18 +0800

    fully functional (w/o keyboard support)

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const numberBtns   = document.querySelectorAll('.num');
 const operatorBtns = document.querySelectorAll('.operator');
+
 const pointBtn     = document.querySelector('#point');
 const clearBtn     = document.querySelector('#clear');
 const deleteBtn    = document.querySelector('#delete');
@@ -7,62 +8,85 @@ const signBtn      = document.querySelector('#sign');
 const equalBtn     = document.querySelector('#equal');
 const display      = document.querySelector('#display');
 
-let firstNum = '';
-let secondNum = '';
-let currentOperator = null;
-let shouldResetDisplay = false;
+equalBtn. addEventListener('click', evaluate);
+clearBtn. addEventListener('click', clear);
+deleteBtn.addEventListener('click', backSpace);
+pointBtn. addEventListener('click', decimalPoint);
+signBtn.  addEventListener('click', negate);
 
 numberBtns.forEach((button) => 
-  button.addEventListener('click', () => inputNum(button.id))
+  button.addEventListener('click', () => inputNum(button.textContent))
 )
 
 operatorBtns.forEach((button) => 
   button.addEventListener('click', () => setOperator(button.textContent))
 )
 
-equalBtn.addEventListener('click', evaluate);
-clearBtn.addEventListener('click', clear)
-
-
-inputNum = (num) => {
-  if (display.textContent === '0' || shouldResetScreen) resetDisplay();
-  display.textContent += num;
-}
-
-setOperator = (operator) => {
-  if (currentOperator !== null) evaluate();
-  firstNum = display.textContent;
-  currentOperator = operator;
-  shouldResetScreen = true;
-}
-
-resetDisplay = () => {
-  display.textContent = "";
-  shouldResetScreen = false;
-}
+let firstNum           = '';
+let secondNum          = '';
+let currentOperator    = null;
+let shouldResetDisplay = false;
 
 function clear () {
   display.textContent = '0'
-  firstNum = '';
-  secondNum = '';
-  currentOperator = null;
+  firstNum            = '';
+  secondNum           = '';
+  currentOperator     = null;
+}
+
+function backSpace () {
+  display.textContent = display.textContent.slice(0, -1);
+}
+
+function decimalPoint () {
+  if (shouldResetDisplay) resetDisplay();
+  if (display.textContent === '') display.textContent = '0';
+  if (display.textContent.includes('.')) return;
+  display.textContent += '.';
+}
+
+function negate () {
+  if (display.textContent.includes('-')) {
+    display.textContent = display.textContent.slice(1);
+  } else {
+    display.textContent = `-${display.textContent}`;
+  }
 }
 
 function evaluate () {
-  if (currentOperator === null || shouldResetScreen) return;
+  if (currentOperator === null || shouldResetDisplay) return;
   if (currentOperator === '/' && display.textContent === '0') {
     alert(`You can't do that, mate`);
     clear();
     return;
   }
     
-  secondNum = display.textContent;
-  display.textContent = `${operate(currentOperator, firstNum, secondNum)}`;
-  currentOperator = null;
+  secondNum           = display.textContent;
+  display.textContent = roundUp(operate(currentOperator, firstNum, secondNum));
+  currentOperator     = null;
 }
 
+inputNum = (num) => {
+  if (display.textContent === '0' || shouldResetDisplay) resetDisplay();
+  display.textContent += num;
+}
 
+setOperator = (operator) => {
+  if (currentOperator !== null) evaluate();
 
+  firstNum           = display.textContent;
+  currentOperator    = operator;
+  shouldResetDisplay = true;
+}
+
+roundUp = (num) => {
+  return Math.round(num * 100)/100;
+}
+
+resetDisplay = () => {
+  display.textContent = "";
+  shouldResetDisplay  = false;
+}
 
 add      = (num1, num2) => {return num1 + num2;}
 subtract = (num1, num2) => {return num1 - num2;}
@@ -82,7 +106,7 @@ operate = (operator, num1, num2) => {
       return multiply(num1, num2);
     case '/' : 
       if (num2 === 0) return null;
-      return divide(num1, num2);
+      else return divide(num1, num2);
     default: 
       return null;
   }
